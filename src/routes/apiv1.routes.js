dcd87d0aaea674505529df0a8c6fdbc1d188b5a6
Cecilia Garcia <134348618+cecilia-garcia-are@users.noreply.github.com>
2023-10-03 22:01:18 -0600

    chat-appi

---
+++ src/routes/apiv1.routes.js
@@ -0,0 +1,11 @@
+const userRoutes = require('../modules/user/user.routes');
+const conversationRoutes = require('../modules/conversations/conversation.routes');
+const messageRoutes= require('../modules/messages/message.routes')
+
+const apiv1Routes = (app) => {
+    app.use('/api/v1/users', userRoutes)
+    app.use('/api/v1/conversations', conversationRoutes)
+    app.use('/api/v1/messages', messageRoutes)
+}
+
+module.exports = apiv1Routes
